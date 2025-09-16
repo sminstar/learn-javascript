dcda776230e1de934e291f985a7af1fbce439952
sminstar <sminaplus@gmail.com>
2025-09-16 09:53:43 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const username = "smin";//상수
+let usermbti = "ISFP";//변수
+console.log("🚀 ~ usermbti:", usermbti);
+
+// console.log("Hello world");
+// console.log(username);
+// console.log(usermbti);
+// console.log("usermbti", usermbti);
+
+
+usermbti = "ISTP";
+console.log("🚀 ~ 변경된 usermbti:", usermbti)
