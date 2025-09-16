fafd759549df591d39fe29b8b6049619efaa2779
sminstar <sminaplus@gmail.com>
2025-09-16 11:45:13 +0900

    함수 매개변수 및 리턴 사용

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const username = "smin";//상수
 let usermbti = "ISFP";//변수
-console.log("🚀 ~ usermbti:", usermbti);
+// console.log("🚀 ~ usermbti:", usermbti);
 
 // console.log("Hello world");
 // console.log(username);
@@ -8,8 +8,8 @@ console.log("🚀 ~ usermbti:", usermbti);
 // console.log("usermbti", usermbti);
 
 
-usermbti = "ISTP";
-console.log("🚀 ~ 변경된 usermbti:", usermbti)
+// usermbti = "ISTP";
+// console.log("🚀 ~ 변경된 usermbti:", usermbti);
 
 
 const number = 12;
@@ -18,10 +18,10 @@ const bool = true;
 const empty = undefined;
 
 
-console.log("num", typeof num);
-console.log("str", typeof str);
-console.log("bool", typeof bool);
-console.log("empty", typeof empty);
+// console.log("num", typeof num);
+// console.log("str", typeof str);
+// console.log("bool", typeof bool);
+// console.log("empty", typeof empty);
 
 /**
  * null과  undifined 차이
@@ -30,3 +30,48 @@ console.log("empty", typeof empty);
  * 
  */
 
+
+console.log("==========================  함수  ==============================");
+
+//함수
+
+//함수 정의/선언
+function greeting() {
+
+  console.log("반갑습니다! " + username);
+  // console.log(`반갑습니다. ${username}님, 당신의 MBTI는 ${usermbti}시군요.`);
+
+}
+
+
+//함수 호출/실행
+greeting();
+
+//함수의 인자와 매개변수
+const sum = (a, b) => {
+
+  const result = a + b;
+  // console.log("result:" + result)
+  return result;
+}
+
+//미션
+
+const sayHello = (param_name) => {
+
+  console.log(`반갑습니다. ${param_name}님!`);
+  
+}
+
+sayHello(username);
+
+const double = (num) => {
+
+  const result = num * 2;
+  return result;
+}
+
+const add = sum(11, 22);
+
+const doubleResult = double(add);
+console.log(doubleResult);
